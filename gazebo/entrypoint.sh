aaf2ae5c2495cecdb13e5d99bc424bd41a71aea9
anilkusc <anilkuscu95@gmail.com>
2023-11-05 18:53:25 +0300

    feat: :art: Parameterized world name

--- gazebo/entrypoint.sh
+++ gazebo/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 gzserver --verbose &
-gz model --model-name double_pendulum --spawn-file double_pendulum.sdf
+gz model --model-name $WORLD_NAME --spawn-file "$WORLD_NAME".sdf
 sleep infinity
