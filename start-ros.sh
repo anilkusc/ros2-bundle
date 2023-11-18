c6b761af076c5d4cff43daba2788f2b514d42531
anilkusc <anilkuscu95@gmail.com>
2023-11-18 14:19:48 +0300

    fix: :hammer: fix start script

--- start-ros.sh
+++ start-ros.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #set -xe
+local_path=$(pwd)
+pkill -f ros_gz_bridge
 rm -fr ros2_ws
 source /opt/ros/iron/setup.sh
 src_folders=$(ls -d src/*/)
@@ -14,20 +16,19 @@ for bridge in $bridges; do
     ros2 run ros_gz_bridge parameter_bridge $bridge & 
 done
 
-mkdir -p ros2_ws/src
-cd ros2_ws/src
+mkdir -p $local_path/ros2_ws/src
+cd $local_path/ros2_ws/src
 ros2 pkg create --build-type ament_python $package_name
-cd ../..
+cd $local_path
 cp -fr src/$package_name ros2_ws/src/$package_name
 cp src/package.xml ros2_ws/src/$package_name/package.xml
 cp src/setup.py ros2_ws/src/$package_name/setup.py
-cd ros2_ws/src/$package_name
+cd $local_path/ros2_ws/src/$package_name
 sed -i "s/<package-name>/$package_name/g" package.xml
 sed -i "s/<package-name>/$package_name/g" setup.py
 pip3 install -r $package_name/requirements.txt
-cd ..
+cd $local_path/ros2_ws/
 rosdep install -i --from-path src --rosdistro iron -y
 colcon build --packages-select $package_name
-
 source install/setup.bash
 ros2 run $package_name $package_name
