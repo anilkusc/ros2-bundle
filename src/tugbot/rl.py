633ba7ae2a90317a07c0ba77d3ddbffc2460a999
anilkusc <anilkuscu95@gmail.com>
2024-01-14 12:57:10 +0300

    feat: :sparkles: added pose

--- src/tugbot/rl.py
+++ src/tugbot/rl.py
@@ -1,7 +1,6 @@
 import os
 import rclpy
-from geometry_msgs.msg import Twist, Vector3
-
+from geometry_msgs.msg import Vector3
 class RL():
     def __init__(self,node) -> None:
         self.state_dim = 1
@@ -13,11 +12,9 @@ class RL():
     def reset(self):
         os.system("gz service -s /world/world_demo/control --reqtype gz.msgs.WorldControl --reptype gz.msgs.Boolean --timeout 3000 --req 'reset: {all: true}'")
 
-
-
     def step(self,action):
         self.node.linear ,self.node.angular = self.calculate_cmd_vel(action)
-        print(self.node.battery_state)
+        print(self.node.pose_position_z)
         rclpy.spin_once(self.node)
     
     def calculate_cmd_vel(self,action):
--- src/tugbot/tugbot_node.py
+++ src/tugbot/tugbot_node.py
@@ -1,8 +1,7 @@
 from rclpy.node import Node
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist, Vector3 , Pose
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import BatteryState
-import time 
 
 class TugbotNode(Node):
 
@@ -13,29 +12,59 @@ class TugbotNode(Node):
         self.subscription_battery # prevent unused variable warning
         self.subscription_odometry = self.create_subscription(Odometry, '/model/tugbot/odometry', self.odometry_callback, 10)
         self.subscription_odometry # prevent unused variable warning
+        self.subscription_pose = self.create_subscription(Pose, '/model/tugbot/pose', self.pose_callback, 10)
+        self.subscription_pose # prevent unused variable warning
+        #self.subscription_tf = self.create_subscription(TransformStamped, '/model/tugbot/tf', self.tf_callback, 10)
+        #self.subscription_tf # prevent unused variable warning
         self.linear = Vector3(x=1.0)
         self.angular = Vector3(z=1.0)
         self.timer_period = 1  # seconds
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
-        self.odometry = None
+        #self.tf_pose_position_x = None
+        self.odometry_pose_pose_position_x = None
+        self.odometry_pose_pose_position_y = None
+        self.odometry_pose_pose_orientation_z = None
+        self.odometry_pose_pose_orientation_w = None
+        self.odometry_twist_twist_linear_x = None
+        self.odometry_twist_twist_angular_y = None
+        self.pose_position_x = None
+        self.pose_position_y = None
+        self.pose_position_z = None
+        self.pose_orientation_x = None
+        self.pose_orientation_y = None
+        self.pose_orientation_z = None
+        self.pose_orientation_w = None
         self.battery_state = None
 
     def timer_callback(self):
         twist_msg = Twist()
         twist_msg.linear = self.linear
         twist_msg.angular = self.angular
-        self.publisher.publish(twist_msg)
+        #self.publisher.publish(twist_msg)
         #self.get_logger().info('Published: "%s"' % twist_msg)
 
+    #def tf_callback(self, tf_msg):
+    #    self.tf_pose_position_x = tf_msg.pose.position.x
+        
+    def pose_callback(self, pose_msg):
+        self.pose_position_x = pose_msg.position.x
+        self.pose_position_y = pose_msg.position.y
+        self.pose_position_z = pose_msg.position.z
+        self.pose_orientation_x = pose_msg.orientation.x
+        self.pose_orientation_y = pose_msg.orientation.y
+        self.pose_orientation_z = pose_msg.orientation.z
+        self.pose_orientation_w = pose_msg.orientation.w
+
     def odometry_callback(self, odometry_msg):
-        #self.get_logger().info('Listened: "%s"' % odometry_msg)
-        #self.linear = Vector3(x=1.0)
-        #self.angular = Vector3(z=1.0)
-        self.odometry = odometry_msg
-        #time.sleep(self.timer_period)
+        self.odometry_pose_pose_position_x = odometry_msg.pose.pose.position.x
+        self.odometry_pose_pose_position_y = odometry_msg.pose.pose.position.y
+        self.odometry_pose_pose_orientation_z = odometry_msg.pose.pose.orientation.z
+        self.odometry_pose_pose_orientation_w = odometry_msg.pose.pose.orientation.w
+        self.odometry_twist_twist_linear_x = odometry_msg.twist.twist.linear.x
+        self.odometry_twist_twist_angular_y = odometry_msg.twist.twist.angular.y
 
     def battery_callback(self, battery_state_msg):
-        self.battery_state = battery_state_msg
+        self.battery_state = battery_state_msg.percentage
 
 #gz service -s /world/world_demo/control --reqtype gz.msgs.WorldControl --reptype gz.msgs.Boolean --timeout 3000 --req 'reset: {all: true}'
 #gz topic -t /model/tugbot/cmd_vel -m gz.msgs.Twist --pub "linear { x: 1 } angular { z: 0.1 }"
